b9a3edc334eaef225e145527f698e6b98722b4f1
Brian Hart <bhart@ypm.com>
2024-08-08 14:39:21 -0700

    alphabetized stop words and added a few

--- WordPress/GravityForms/spam-blocking-filter.php
+++ WordPress/GravityForms/spam-blocking-filter.php
@@ -1,95 +1,112 @@
-add_filter( 'gform_field_validation_1', 'filter_words', 10, 4 ); // applies to all fields for form id 1
-add_filter( 'gform_field_validation_1', 'cyrillic_greek_validation', 10, 4 ); // applies to all fields for form id 1
-add_filter( 'gform_field_validation_1_2', 'filter_email_results', 10, 4 ); // applies to form 1 field ID 2 only
+<?php
 
-function filter_words( $result, $value, $form, $field ) {
-	GFCommon::log_debug( __METHOD__ . '(): Running...' );
+add_filter('gform_field_validation_1', 'filter_words', 10, 4); // applies to all fields for form id 1
+add_filter('gform_field_validation_1', 'cyrillic_greek_validation', 10, 4); // applies to all fields for form id 1
+add_filter('gform_field_validation_1_2', 'filter_email_results', 10, 4); // applies to form 1 field ID 2 only
+
+function filter_words($result, $value, $form, $field)
+{
+	GFCommon::log_debug(__METHOD__ . '(): Running...');
 	// Only for Single Line Text and Paragraph fields.
-	if ( $field->type == 'text' || $field->type == 'textarea' ) {
-  
-		if ( $result['is_valid'] ) {
-			$stop_words = array( // List of words to not allow in lowercase.
-				'viagra',
-				'porn',
-				'sidenafil',
+	if ($field->type == 'text' || $field->type == 'textarea') {
+
+		if ($result['is_valid']) {
+			$stop_words = array(
+				'betting',
+				'bit.ly',
+				'casino',
+				'ciao',
 				'cialis',
-				'seo',
-				'sex',
-				'rank',
-				'rankings',
-				'leads',
-				'trial',
-				'software',
-				'virus',
-				'viruses',
-				'malware',
-				'robertked',
-				'venture capital',
-				'skype',
-				'whatsapp',
+				'crypto',
+				'enlic',
 				'feedback forms',
 				'feedbackform',
 				'file sharing',
-				'online security',
-				'spam',
 				'funding',
+				'guardian',
 				'href=',
-				'url=',
-				'instagram',
-				'crypto',
+				'hosting',
+				'jackpot',
+				'kaenquirynicholls',
+				'kiddishop',
+				'leads',
+				'malware',
+				'music',
+				'nudes',
+				'online security',
+				'porn',
+				'pussy',
+				'rb.gy',
+				'rank',
+				'rankings',
+				'registry.godaddy',
+				'robertked',
+				's3x',
+				'sawubona',
+				'seo',
+				'sex',
+				'sidenafil',
+				'skype',
+				'spam',
+				'stop_words',
+				'sveiki',
 				'telegram',
-				'youtube',
-				'yandex',
-				'godaddy',
-				'mtskheta',
-				'robertwheme'
+				'trial',
+				'url=',
+				'venture capital',
+				'viagra',
+				'virus',
+				'viruses',
+				'whatsapp',
+				'yandex'
 			);
-  
+
 			// Stop Words Counter.
 			$stop_words_detected = 0;
-  
+
 			// Check field value for Stop Words.
-			foreach ( $stop_words as $stop_word ) {
-				if ( strpos( strtolower( $value ), $stop_word ) !== false ) {
+			foreach ($stop_words as $stop_word) {
+				if (strpos(strtolower($value), $stop_word) !== false) {
 					$stop_words_detected++;
-					GFCommon::log_debug( __METHOD__ . "(): Increased Stop Words counter for field id {$field->id}. Stop Word: {$stop_word}" );
+					GFCommon::log_debug(__METHOD__ . "(): Increased Stop Words counter for field id {$field->id}. Stop Word: {$stop_word}");
 				}
 			}
-  
-			if ( $stop_words_detected > 0 ) {
-				GFCommon::log_debug( __METHOD__ . "(): {$stop_words_detected} Stop words detected." );
+
+			if ($stop_words_detected > 0) {
+				GFCommon::log_debug(__METHOD__ . "(): {$stop_words_detected} Stop words detected.");
 				$result['is_valid'] = false;
 				$result['message']  = 'Sorry, there is a problem with your message. Please try again.';
 			}
 		}
-  
 	}
-  
-	return $result;
-  }
 
-function cyrillic_greek_validation( $result, $value, $form, $field ) {
-	GFCommon::log_debug( __METHOD__ . '(): running for field type ' . $field->type );
-	if ( 'text' !== $field->type && 'textarea' !== $field->type ) {
-		GFCommon::log_debug( __METHOD__ . '(): No text or paragraph field.' );
+	return $result;
+}
+
+function cyrillic_greek_validation($result, $value, $form, $field)
+{
+	GFCommon::log_debug(__METHOD__ . '(): running for field type ' . $field->type);
+	if ('text' !== $field->type && 'textarea' !== $field->type) {
+		GFCommon::log_debug(__METHOD__ . '(): No text or paragraph field.');
 		return $result;
 	}
 
 	// Cyrillic & Greek check.
-	$cyrillic = preg_match( '/[\p{Cyrillic}]/u', $value);
-	$greek = preg_match( '/[\p{Greek}]/u', $value);
+	$cyrillic = preg_match('/[\p{Cyrillic}]/u', $value);
+	$greek = preg_match('/[\p{Greek}]/u', $value);
 
-	if ( $result['is_valid'] && ( $cyrillic || $greek ) ) {
-		GFCommon::log_debug( __METHOD__ . '(): Cyrillic or Greek detected!' );
+	if ($result['is_valid'] && ($cyrillic || $greek)) {
+		GFCommon::log_debug(__METHOD__ . '(): Cyrillic or Greek detected!');
 		$result['is_valid'] = false;
 		$result['message'] = 'Sorry, there is a problem with your message. Please try again.';
 	}
 
 	return $result;
 }
-  
-function filter_email_results( $result, $value, $form, $field ) {
-	if ( $result['is_valid'] ) {
+
+function filter_email_results($result, $value, $form, $field)
+{
+	if ($result['is_valid']) {
 		$stop_words = array( // List of words to not allow in lowercase.
 			'.ru',
 			'.store',
@@ -100,15 +117,15 @@ function filter_email_results( $result, $value, $form, $field ) {
 		$stop_words_detected = 0;
 
 		// Check field value for Stop Words.
-		foreach ( $stop_words as $stop_word ) {
-			if ( strpos( strtolower( $value ), $stop_word ) !== false ) {
+		foreach ($stop_words as $stop_word) {
+			if (strpos(strtolower($value), $stop_word) !== false) {
 				$stop_words_detected++;
-				GFCommon::log_debug( __METHOD__ . "(): Increased Stop Words counter for field id {$field->id}. Stop Word: {$stop_word}" );
+				GFCommon::log_debug(__METHOD__ . "(): Increased Stop Words counter for field id {$field->id}. Stop Word: {$stop_word}");
 			}
 		}
 
-		if ( $stop_words_detected > 0 ) {
-			GFCommon::log_debug( __METHOD__ . "(): {$stop_words_detected} Stop words detected." );
+		if ($stop_words_detected > 0) {
+			GFCommon::log_debug(__METHOD__ . "(): {$stop_words_detected} Stop words detected.");
 			$result['is_valid'] = false;
 			$result['message']  = 'Sorry, there is a problem with your email. Please try again.';
 		}
@@ -116,11 +133,12 @@ function filter_email_results( $result, $value, $form, $field ) {
 	return $result;
 }
 
-add_action( 'gform_pre_submission', 'ip_pre_submission_handler'); // blocks submission of all forms if a matching IP
-function ip_pre_submission_handler( $form ) {
+add_action('gform_pre_submission', 'ip_pre_submission_handler'); // blocks submission of all forms if a matching IP
+function ip_pre_submission_handler($form)
+{
 	$blocked_ips = array('85.209.11.20', '85.209.11.117');
 	$ip_address = $_SERVER['REMOTE_ADDR'];
 	if (in_array($ip_address, $blocked_ips)) {
-		die( "ERROR: Please contact the webmaster." );
+		die("ERROR: Please contact the webmaster.");
 	}
 }
