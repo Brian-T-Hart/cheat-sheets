e7fab739ad5707908e59eb87506a083afc77bcdc
Brian Hart <bhart@ypm.com>
2025-04-30 13:24:59 -0700

    Updated conditional

--- WordPress/Helper_Functions/disable-user-endpoints.php
+++ WordPress/Helper_Functions/disable-user-endpoints.php
@@ -1,12 +1,9 @@
 <?php
 
-/**
- * Disable the WordPress REST API users endpoint for non-logged-in users.
- * This is a security measure to prevent unauthorized access to user data.
- */
+/* Block WP REST API access to users */
 add_filter('rest_authentication_errors', function ($result) {
-  if (! is_user_logged_in()) {
-    if (strpos($_SERVER['REQUEST_URI'], '/wp/v2/users') !== false) {
+  if (strpos($_SERVER['REQUEST_URI'], '/wp/v2/users') !== false) {
+    if (! is_user_logged_in() || ! current_user_can('list_users')) {
       return new WP_Error('rest_cannot_access', 'User data is protected.', array('status' => 403));
     }
   }
