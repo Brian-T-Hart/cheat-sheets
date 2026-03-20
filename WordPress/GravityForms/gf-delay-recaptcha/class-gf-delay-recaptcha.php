017937a3970b6c388fc7b53c895e0e8696512087
Brian Hart <bhart@ypm.com>
2026-03-20 13:23:48 -0700

    Works in any directory in the child theme

--- WordPress/GravityForms/gf-delay-recaptcha/class-gf-delay-recaptcha.php
+++ WordPress/GravityForms/gf-delay-recaptcha/class-gf-delay-recaptcha.php
@@ -6,6 +6,11 @@
  */
 class GFDelayReCaptcha
 {
+    private static $version = '1.0.0';
+    private static $script_paths = [
+        'minjs' => 'gf-delay-recaptcha.min.js',
+        'js' => 'gf-delay-recaptcha.js'
+    ];
 
     /**
      * Initialize the class and hooks
@@ -21,7 +26,7 @@ class GFDelayReCaptcha
     public static function enqueue_recaptcha_delay_script($form, $is_ajax)
     {
         $form_id = $form['id'];
-        $timestamp = time();
+        // $timestamp = time();
         $recaptcha_src = '';
 
         global $wp_scripts;
@@ -29,17 +34,36 @@ class GFDelayReCaptcha
             $recaptcha_src = $wp_scripts->registered['gform_recaptcha']->src;
         }
 
-        add_filter('script_loader_src', [__CLASS__, 'remove_recaptcha_src'], 10, 2);
+        // Get the directory of this file dynamically
+        $current_dir = __DIR__;
+        $stylesheet_dir = get_stylesheet_directory();
 
-        // check if the minified version of the script exists, if not use the non-minified version
-        $script_path = '/functions/gf-delay-recaptcha/gf-delay-recaptcha.min.js';
-        if (!file_exists(get_stylesheet_directory() . $script_path)) {
-            $script_path = '/functions/gf-delay-recaptcha/gf-delay-recaptcha.js';
+        // Calculate relative path from stylesheet to this directory
+        $relative_path = str_replace(wp_normalize_path($stylesheet_dir), '', wp_normalize_path($current_dir));
+
+        // Check for minified version first, then fallback to non-minified
+        $script_filename = self::$script_paths['minjs'];
+        $script_path = $relative_path . '/' . $script_filename;
+        $full_path = $stylesheet_dir . $script_path;
+
+        if (!file_exists($full_path)) {
+            $script_filename = self::$script_paths['js'];
+            $script_path = $relative_path . '/' . $script_filename;
+            $full_path = $stylesheet_dir . $script_path;
+
+            if (!file_exists($full_path)) {
+                GFCommon::log_debug(__METHOD__ . "(): Error - Script file not found at " . $full_path);
+                return;
+            }
         }
 
+        // Stop the reCAPTCHA script from loading by removing its src
+        add_filter('script_loader_src', [__CLASS__, 'remove_recaptcha_src'], 10, 2);
+
+        // Enqueue our custom script to handle the delayed loading of reCAPTCHA
         wp_enqueue_script(
             'gf-delay-recaptcha',
-            get_stylesheet_directory_uri() . $script_path . '?v=' . $timestamp,
+            get_stylesheet_directory_uri() . $script_path . '?v=' . self::$version,
             array(),
             null,
             true
@@ -49,10 +73,11 @@ class GFDelayReCaptcha
         $localized_data = [
             'formId' => $form_id,
             'ajaxEnabled' => $is_ajax,
-            'recaptchaSrc' => $recaptcha_src,
+            'recaptchaSrc' => esc_url($recaptcha_src)
         ];
 
         wp_localize_script('gf-delay-recaptcha', 'GFDelayReCaptchaData', $localized_data);
+
 
         GFCommon::log_debug(__METHOD__ . "(): Enqueued script for form ID {$form_id}.");
     } // enqueue_recaptcha_delay_script
@@ -64,7 +89,7 @@ class GFDelayReCaptcha
     {
         if ($handle === 'gform_recaptcha') {
 
-            GFCommon::log_debug(__METHOD__ . "(): Script src = {self::$src}.");
+            GFCommon::log_debug(__METHOD__ . "(): Removed reCAPTCHA script src {$src}.");
 
             $src = "";
         }
